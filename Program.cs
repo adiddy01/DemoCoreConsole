f8d1333ac6f2e6904733b1a8fb66c191954d01ae
adiddy01 <adiddy01@gmail.com>
2019-03-22 14:02:17 -0700

    added student class

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace DemoConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World, this is the experimental branch. Experiences may vary.");
+            Student c = new Student();
+            c.Name = "Joe Smith";
+            c.Id = 4;
+            Console.WriteLine(c.Name);
         }
     }
 }
