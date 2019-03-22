d5892f91ac225cdb087a571f062b9da22148cbb9
adiddy01 <adiddy01@gmail.com>
2019-03-22 13:57:46 -0700

    added experiment text

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DemoConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World, I have installed Git");
+            Console.WriteLine("Hello World, this is the experimental branch. Experiences may vary.");
         }
     }
 }
